793dfc3cba4e882cbf646d58de9f4184ef023b40
Figueiredodev777 <rafaelmagalhaesfigue@gmail.com>
2025-05-05 11:12:26 -0400

    Update tmb.c

--- tmb.c
+++ tmb.c
@@ -1,28 +1,42 @@
+#include <stdio.h>
  int main(){
  	
     float peso, altura, idade;
     float resultado;
+ 	int escolha;
  	
  	printf("Digite uma idade:\n");
  	scanf("%f", &idade);
  	
-	printf("Digite seu peso e kilo:\n");
- 	scanf("%2f", &peso);
+	printf("Digite seu peso em kilo:\n");
+ 	scanf("%f", &peso);
  	
  	printf("Digite sua altura em centimetros:\n");
  	scanf("%f", &altura);
  	
  	printf("Idade informada: %f.\n", idade);
- 	printf("Peso informado: %2f.\n", peso);
+ 	printf("Peso informado: %f.\n", peso);
  	printf("Altura informado: %f.\n", altura);
  		
+ 	printf("Escolha o sexo:\n");
+        printf("1 - Homem\n");
+        printf("2 - Mulher\n");
+        printf("digite o numero:");
+        scanf("%d", &escolha);
  	
  	
- 	resultado = 66.5 + (13.75 * peso) + (5 * altura) - (6.75 * idade);
+ 	if (escolha == 1) {
+        
+       resultado = 66.5 + (13.75 * peso) + (5 * altura) - (6.75 * idade);
+        printf("sua taxa metabolica basal e: %.2f calorias/dia.\n", resultado);
+    } else if (escolha == 2) {
+        
+        resultado = 655 + (9.56 * peso) + (1.85 * altura) - (4.68 * idade);
+        printf("sua taxa metabolica basal e: %.2f calorias/dia.\n", resultado);
+    } 
+        
+        
+    
  	
- 	printf("Sua taxa metabolica basal: %.2f calorias/dia\n", resultado);
- 	
-//vou mudar a variavel, para q a formula mude de acordo com o sexo// 
-
 return 0;
  } 
